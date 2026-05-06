1c66dd78fed493bbbec4f33008279bb4a3a5eca1
areyes2019 <reyesabdias@gmail.com>
2026-05-06 13:40:05 -0600

    arreglamos el problema

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/public/' : '/',
+  base: '/',
   plugins: [
     vue(),
     tailwindcss(),
